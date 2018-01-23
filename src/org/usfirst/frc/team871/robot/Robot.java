45f625218bc2b9ab6fe21c8345bf869d8bd67c0c
Team871 <Team871@DESKTOP-LOB2H26>
2018-01-22 20:06:05 -0500

    Initial commit

---
+++ src/org/usfirst/frc/team871/robot/Robot.java
@@ -0,0 +1,36 @@
+
+//TODO: MAKE VARS AN INTERFACE THAT CAN  SWICTH BETWEEN EACH ROBOT INTERFACES
+
+package org.usfirst.frc.team871.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class Robot extends IterativeRobot {
+
+	@Override
+	public void robotInit() {
+		
+	}
+
+	@Override
+	public void autonomousInit() {
+		
+	}
+
+	@Override
+	public void autonomousPeriodic() {
+		
+	}
+
+	@Override
+	public void teleopPeriodic() {
+		
+	}
+
+	@Override
+	public void testPeriodic() {
+		
+	}
+}

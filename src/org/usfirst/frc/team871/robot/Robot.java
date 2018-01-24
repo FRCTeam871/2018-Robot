80724cdf5f558e5b8b4f2a4927a403cffbcc9a19
Team871 <Team871@DESKTOP-LOB2H26>
2018-01-23 20:40:28 -0500

    Implemented drive train
    -Added joystick drive controls.
    -Added button to toggle between field and robot oriented as well as one
    to reset the gyroscope.
    
    	SCOTT :(

--- src/org/usfirst/frc/team871/robot/Robot.java
+++ src/org/usfirst/frc/team871/robot/Robot.java
@@ -3,13 +3,29 @@
 
 package org.usfirst.frc.team871.robot;
 
+import org.usfirst.frc.team871.subsystems.DriveTrain;
+import org.usfirst.frc.team871.util.config.IRobotConfiguration;
+import org.usfirst.frc.team871.util.config.MainRobotConfiguration;
+import org.usfirst.frc.team871.util.joystick.ButtonTypes;
+import org.usfirst.frc.team871.util.joystick.EnhancedXBoxController;
+import org.usfirst.frc.team871.util.joystick.XBoxAxes;
+import org.usfirst.frc.team871.util.joystick.XBoxButtons;
+
 import edu.wpi.first.wpilibj.IterativeRobot;
 
 public class Robot extends IterativeRobot {
 		
+	private DriveTrain drive;
+	private IRobotConfiguration config;
+	private EnhancedXBoxController xbox;
+	
 	@Override
 	public void robotInit() {
-
+		xbox = new EnhancedXBoxController(0);
+		config = MainRobotConfiguration.getConfiguration();
+		drive = new DriveTrain(config.getRearRightMotor(), config.getRearLeftMotor(), config.getFrontRightMotor(), config.getFrontLeftMotor(), config.getGyroscope());
+		xbox.setButtonMode(XBoxButtons.START, ButtonTypes.TOGGLE);
+		xbox.setButtonMode(XBoxButtons.BACK, ButtonTypes.RISING);
 	}
 
 	@Override
@@ -19,12 +35,20 @@ public class Robot extends IterativeRobot {
 
 	@Override
 	public void autonomousPeriodic() {
-		
+
 	}
 
 	@Override
 	public void teleopPeriodic() {
+		if (xbox.getValue(XBoxButtons.START)) {
+			drive.driveRobotOriented(xbox.getValue(XBoxAxes.LEFTX), xbox.getValue(XBoxAxes.LEFTY), xbox.getValue(XBoxAxes.RIGHTX));
+		} else {
+			drive.driveFieldOriented(xbox.getValue(XBoxAxes.LEFTX), xbox.getValue(XBoxAxes.LEFTY), xbox.getValue(XBoxAxes.RIGHTX));
+		}
 		
+		if (xbox.getValue(XBoxButtons.BACK)) {
+			drive.resetGyro();
+		}
 	}
 
 	@Override

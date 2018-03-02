f320d1e75872541623e29cdc6ba66de67dafded0
Team871 <abaranec@optonline.net>
2018-03-01 20:57:25 -0500

    scott is dumb

---
+++ src/org/usfirst/frc/team871/subsystems/navigation/WaypointArrayPositionWrapper.java
@@ -0,0 +1,33 @@
+package org.usfirst.frc.team871.subsystems.navigation;
+
+public class WaypointArrayPositionWrapper {
+
+	private final Waypoint[] waypoints;
+	private final WaypointPosition startPosition;
+	private final WaypointPosition endPosition;
+	private final WaypointSide side;
+	
+	public WaypointArrayPositionWrapper(Waypoint[] waypoints, WaypointPosition startPosition, WaypointPosition endPosition, WaypointSide side) {
+		this.waypoints = waypoints;
+		this.startPosition = startPosition;
+		this.endPosition = endPosition;
+		this.side = side;
+	}
+
+	public Waypoint[] getWaypoints() {
+		return waypoints;
+	}
+
+	public WaypointPosition getStartPosition() {
+		return startPosition;
+	}
+
+	public WaypointPosition getEndPosition() {
+		return endPosition;
+	}
+
+	public WaypointSide getSide() {
+		return side;
+	}
+	
+}
